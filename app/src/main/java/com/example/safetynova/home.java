105b981eb4f01fcb4a3f9c73c694b67049125b94
NishthaV <vartaknishtha@gmail.com>
2025-01-03 14:46:47 +0530

    commit27

--- app/src/main/java/com/example/safetynova/home.java
+++ app/src/main/java/com/example/safetynova/home.java
@@ -4,6 +4,7 @@ package com.example.safetynova;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class home extends AppCompatActivity {
 
     private ImageButton btnhome,btnmap;
+    private ImageView btnuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class home extends AppCompatActivity {
         loadFragment(new HomeFragment());
         btnmap=findViewById(R.id.nav_maps);
         btnhome=findViewById(R.id.nav_home);
+        btnuser=findViewById(R.id.user_icon);
         btnhome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +34,12 @@ public class home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 loadFragment(new MapFragment());
+            }
+        });
+        btnuser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) { loadFragment(new Profile());
+
             }
         });
     }
