495acb0c8da6930f8b826f286c17a009ad0d8977
NishthaV <vartaknishtha@gmail.com>
2025-01-10 20:41:12 +0530

    Nish32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.google.android.gms:play-services-auth:20.6.0")
 }
