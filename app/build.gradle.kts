9942f3b944fb964abc53903fa99f0e71d1fa1ac1
Aryan Patil <aryanpatil15aug@gmail.com>
2025-01-12 10:33:22 +0530

    A17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.android.gms:play-services-auth:20.6.0")
     implementation("com.google.firebase:firebase-auth:23.1.0") // Firebase Authentication
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
