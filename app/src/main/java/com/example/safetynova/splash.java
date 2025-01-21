6a2042ccc29db9b63fea76de7df2fc4e3fb008f8
kesar <kesar35parab@gmail.com>
2025-01-21 14:47:19 +0530

    K17 A25

--- app/src/main/java/com/example/safetynova/splash.java
+++ app/src/main/java/com/example/safetynova/splash.java
@@ -5,20 +5,37 @@ import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class splash extends AppCompatActivity {
 
     private static final int SPLASH_SCREEN_DURATION = 1000; // Splash screen duration in milliseconds (3 seconds)
+    private FirebaseAuth fAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash); // Reference your splash screen layout
 
+        fAuth = FirebaseAuth.getInstance();
+        user = fAuth.getCurrentUser();
+
         // Using Handler to create a delay
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(splash.this, login.class);
-            startActivity(intent);
-            finish(); // Close this activity so it's not part of the back stack
+            if(user==null || fAuth==null) {
+                Intent intent = new Intent(splash.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+            else
+            {
+                Intent intent = new Intent(splash.this, home.class);
+                startActivity(intent);
+                finish();
+            }
+            // Close this activity so it's not part of the back stack
         }, SPLASH_SCREEN_DURATION);
     }
 }
